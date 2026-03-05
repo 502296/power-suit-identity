8ad4013c413a56eb757f70aac043397e2fa0ab5d
502296 <ali.kathem.edu@gmail.com>
2026-03-05 12:57:39 -0500

    Create list-atelier-images.js

---
+++ api/list-atelier-images.js
@@ -0,0 +1,9 @@
+module.exports = async (req, res) => {
+  // أسماء الصور (ثابتة وبسيطة)
+  const images = [
+    "01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg",
+    "07.jpg","08.jpg","09.jpg","10.jpg","11.jpg","12.jpg"
+  ];
+
+  res.status(200).json({ images });
+};
