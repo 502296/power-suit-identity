8f1db01577efad3e8ed7b15f82d658ec0cc8f7dc
502296 <ali.kathem.edu@gmail.com>
2026-03-06 21:48:54 -0500

    Update list-atelier-images.js

--- api/list-atelier-images.js
+++ api/list-atelier-images.js
@@ -1,15 +1,12 @@
 module.exports = async (req, res) => {
   try {
-
     const images = [];
 
-    // يولد صور من 01 إلى 99
     for (let i = 1; i <= 99; i++) {
       images.push(`${String(i).padStart(2, "0")}.jpg`);
     }
 
     return res.status(200).json({ images });
-
   } catch (e) {
     return res.status(500).json({
       images: [],
